9930ba9e9c7393891c8394970518c63b2a15f8db
Rahul Suwasiya <88681527+rsuwasiya17@users.noreply.github.com>
2021-11-05 00:33:30 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -208,10 +208,8 @@ def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = "s3a://datalake-output17/"
-
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
-
 
 if __name__ == "__main__":
     main()
